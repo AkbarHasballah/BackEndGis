837c7d162b395965454b76dadcc43c649b419149
AkbarHasballah <salmanakbartafisa@gmail.com>
2023-11-15 22:44:45 +0700

    tes

--- BackEndGis.go
+++ BackEndGis.go
@@ -8,44 +8,12 @@ import (
 	"net/http"
 	"os"
 
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-
 	"github.com/aiteung/atdb"
 	"github.com/whatsauth/watoken"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
-func GCFPostkordinatCoy(Mongostring, Publickey, dbname, colname string, r *http.Request) string {
-	req := new(Credents)
-	conn := GetConnectionMongo(Mongostring, dbname)
-	resp := new(LonLatProperties)
-	err := json.NewDecoder(r.Body).Decode(&resp)
-	tokenlogin := r.Header.Get("Login")
-	if tokenlogin == "" {
-		req.Status = strconv.Itoa(http.StatusNotFound)
-		req.Message = "Header Login Not Exist"
-	} else {
-		existing := IsExist(tokenlogin, os.Getenv(Publickey))
-		if !existing {
-			req.Status = strconv.Itoa(http.StatusNotFound)
-			req.Message = "Kamu kayaknya belum punya akun"
-		} else {
-			if err != nil {
-				req.Status = strconv.Itoa(http.StatusNotFound)
-				req.Message = "error parsing application/json: " + err.Error()
-			} else {
-				req.Status = strconv.Itoa(http.StatusOK)
-				Ins := InsertDataLonlat(conn, colname,
-					resp.Coordinates,
-					resp.Name,
-					resp.Volume,
-					resp.Type)
-				req.Message = fmt.Sprintf("%v:%v", "Berhasil Input data", Ins)
-			}
-		}
-	}
-	return ReturnStringStruct(req)
-}
 
 func GCFHandler(MONGOCONNSTRINGENV, dbname, collectionname string) string {
 	mconn := SetConnection(MONGOCONNSTRINGENV, dbname)
@@ -504,4 +472,499 @@ func DeleteWisata(MONGOCONNSTRING, dbname, collectionname string, filter bson.D)
 	}
 
 	return nil
+}
+func InsertDataUserGCF(Mongoenv, dbname string, r *http.Request) string {
+	resp := new(Credential)
+	userdata := new(User)
+	resp.Status = false
+	conn := SetConnection(Mongoenv, dbname)
+	err := json.NewDecoder(r.Body).Decode(&userdata)
+	if err != nil {
+		resp.Message = "error parsing application/json: " + err.Error()
+	} else {
+		resp.Status = true
+		hash, err := HashPassword(userdata.Password)
+		if err != nil {
+			resp.Message = "Gagal Hash Password" + err.Error()
+		}
+		InsertUserdata(conn, userdata.Username, userdata.Role, hash)
+		resp.Message = "Berhasil Input data"
+	}
+	return GCFReturnStruct(resp)
+}
+
+// Content
+
+func GCFCreateContent(MONGOCONNSTRINGENV, dbname, collectionname string, r *http.Request) string {
+	mconn := SetConnection(MONGOCONNSTRINGENV, dbname)
+	var datacontent Content
+	err := json.NewDecoder(r.Body).Decode(&datacontent)
+	if err != nil {
+		return err.Error()
+	}
+
+	CreateNewContent(mconn, collectionname, datacontent)
+	// setelah create content munculkan response berhasil dan 200
+
+	if CreateResponse(true, "Berhasil", datacontent) != (Response{}) {
+		return GCFReturnStruct(CreateResponse(true, "success Create Data Content", datacontent))
+	} else {
+		return GCFReturnStruct(CreateResponse(false, "Failed Create Data Content", datacontent))
+	}
+}
+
+func GCFDeleteHandlerContent(MONGOCONNSTRINGENV, dbname, collectionname string, r *http.Request) string {
+	mconn := SetConnection(MONGOCONNSTRINGENV, dbname)
+	var contentdata Content
+	err := json.NewDecoder(r.Body).Decode(&contentdata)
+	if err != nil {
+		return err.Error()
+	}
+	DeleteContent(mconn, collectionname, contentdata)
+	return GCFReturnStruct(contentdata)
+}
+
+func GCFUpdatedContent(MONGOCONNSTRINGENV, dbname, collectionname string, r *http.Request) string {
+	mconn := SetConnection(MONGOCONNSTRINGENV, dbname)
+	var contentdata Content
+	err := json.NewDecoder(r.Body).Decode(&contentdata)
+	if err != nil {
+		return err.Error()
+	}
+	ReplaceContent(mconn, collectionname, bson.M{"id": contentdata.ID}, contentdata)
+	return GCFReturnStruct(contentdata)
+}
+
+func GCFCreateNewBlog(MONGOCONNSTRINGENV, dbname, collectionname string, r *http.Request) string {
+	mconn := SetConnection(MONGOCONNSTRINGENV, dbname)
+	var blogdata Blog
+	err := json.NewDecoder(r.Body).Decode(&blogdata)
+	if err != nil {
+		return err.Error()
+	}
+	CreateNewBlog(mconn, collectionname, blogdata)
+	return GCFReturnStruct(blogdata)
+}
+
+func GCFFindContentAllID(MONGOCONNSTRINGENV, dbname, collectionname string, r *http.Request) string {
+	mconn := SetConnection(MONGOCONNSTRINGENV, dbname)
+
+	// Inisialisasi variabel datacontent
+	var datacontent Content
+
+	// Membaca data JSON dari permintaan HTTP ke dalam datacontent
+	err := json.NewDecoder(r.Body).Decode(&datacontent)
+	if err != nil {
+		return err.Error()
+	}
+
+	// Memanggil fungsi FindContentAllId
+	content := FindContentAllId(mconn, collectionname, datacontent)
+
+	// Mengembalikan hasil dalam bentuk JSON
+	return GCFReturnStruct(content)
+}
+
+func GCFFindBlogAllID(MONGOCONNSTRINGENV, dbname, collectionname string, r *http.Request) string {
+	mconn := SetConnection(MONGOCONNSTRINGENV, dbname)
+
+	// Inisialisasi variabel datacontent
+	var datablog Blog
+
+	// Membaca data JSON dari permintaan HTTP ke dalam datacontent
+	err := json.NewDecoder(r.Body).Decode(&datablog)
+	if err != nil {
+		return err.Error()
+	}
+
+	// Memanggil fungsi FindContentAllId
+	blog := GetIDBlog(mconn, collectionname, datablog)
+
+	// Mengembalikan hasil dalam bentuk JSON
+	return GCFReturnStruct(blog)
+}
+
+func GCFGetAllBlog(MONGOCONNSTRINGENV, dbname, collectionname string) string {
+	mconn := SetConnection(MONGOCONNSTRINGENV, dbname)
+	datablog := GetAllBlogAll(mconn, collectionname)
+	return GCFReturnStruct(datablog)
+}
+
+func GCFCreateTokenAndSaveToDB(PASETOPRIVATEKEYENV, MONGOCONNSTRINGENV, dbname, collectionname string, r *http.Request) (string, error) {
+	mconn := SetConnection(MONGOCONNSTRINGENV, dbname)
+
+	// Inisialisasi variabel datauser
+	var datauser User
+
+	// Membaca data JSON dari permintaan HTTP ke dalam datauser
+	if err := json.NewDecoder(r.Body).Decode(&datauser); err != nil {
+		return "", err // Mengembalikan kesalahan langsung
+	}
+
+	// Generate a token for the user
+	tokenstring, err := watoken.Encode(datauser.Username, os.Getenv(PASETOPRIVATEKEYENV))
+	if err != nil {
+		return "", err // Mengembalikan kesalahan langsung
+	}
+	datauser.Token = tokenstring
+
+	// Simpan pengguna ke dalam basis data
+	if err := atdb.InsertOneDoc(mconn, collectionname, datauser); err != nil {
+		return tokenstring, nil // Mengembalikan kesalahan langsung
+	}
+
+	return tokenstring, nil // Mengembalikan token dan nil untuk kesalahan jika sukses
+}
+func GCFCreteRegister(MONGOCONNSTRINGENV, dbname, collectionname string, r *http.Request) string {
+	mconn := SetConnection(MONGOCONNSTRINGENV, dbname)
+	var userdata User
+	err := json.NewDecoder(r.Body).Decode(&userdata)
+	if err != nil {
+		return err.Error()
+	}
+	CreateUser(mconn, collectionname, userdata)
+	return GCFReturnStruct(userdata)
+}
+
+func GCFLoginAfterCreate(MONGOCONNSTRINGENV, dbname, collectionname string, r *http.Request) string {
+	mconn := SetConnection(MONGOCONNSTRINGENV, dbname)
+	var userdata User
+	err := json.NewDecoder(r.Body).Decode(&userdata)
+	if err != nil {
+		return err.Error()
+	}
+	if IsPasswordValid(mconn, collectionname, userdata) {
+		tokenstring, err := watoken.Encode(userdata.Username, os.Getenv("PASETOPRIVATEKEYENV"))
+		if err != nil {
+			return err.Error()
+		}
+		userdata.Token = tokenstring
+		return GCFReturnStruct(userdata)
+	} else {
+		return "Password Salah"
+	}
+}
+
+func GCFLoginAfterCreater(MONGOCONNSTRINGENV, dbname, collectionname, privateKeyEnv string, r *http.Request) (string, error) {
+	// Ambil data pengguna dari request, misalnya dari body JSON atau form data.
+	var userdata User
+	// Implement the logic to extract user data from the request (r) here.
+
+	mconn := SetConnection(MONGOCONNSTRINGENV, dbname)
+
+	// Lakukan otentikasi pengguna yang baru saja dibuat.
+	token, err := AuthenticateUserAndGenerateToken(privateKeyEnv, mconn, collectionname, userdata)
+	if err != nil {
+		return "", err
+	}
+	return token, nil
+}
+
+func GCFLoginAfterCreatee(MONGOCONNSTRINGENV, dbname, collectionname string, r *http.Request) string {
+	mconn := SetConnection(MONGOCONNSTRINGENV, dbname)
+	var userdata User
+	err := json.NewDecoder(r.Body).Decode(&userdata)
+	if err != nil {
+		return err.Error()
+	}
+	if IsPasswordValid(mconn, collectionname, userdata) {
+		// Password is valid, return a success message or some other response.
+		return "Login successful"
+
+	} else {
+		// Password is not valid, return an error message.
+		return "Password Salah"
+	}
+}
+
+func GCFLoginAfterCreateee(MONGOCONNSTRINGENV, dbname, collectionname string, r *http.Request) string {
+	mconn := SetConnection(MONGOCONNSTRINGENV, dbname)
+	var userdata User
+	err := json.NewDecoder(r.Body).Decode(&userdata)
+	if err != nil {
+		return err.Error()
+	}
+	if IsPasswordValid(mconn, collectionname, userdata) {
+		// Password is valid, construct and return the GCFReturnStruct.
+		response := CreateResponse(true, "Berhasil Login", userdata)
+		return GCFReturnStruct(response) // Return GCFReturnStruct directly
+	} else {
+		// Password is not valid, return an error message.
+		return "Password Salah"
+	}
+}
+func GCFLoginAfterCreateeee(MONGOCONNSTRINGENV, dbname, collectionname string, r *http.Request) string {
+	mconn := SetConnection(MONGOCONNSTRINGENV, dbname)
+	var userdata User
+	err := json.NewDecoder(r.Body).Decode(&userdata)
+	if err != nil {
+		return err.Error()
+	}
+	if IsPasswordValid(mconn, collectionname, userdata) {
+		// Password is valid, return a success message or some other response.
+		return GCFReturnStruct(userdata)
+	} else {
+		// Password is not valid, return an error message.
+		return "Password Salah"
+	}
+}
+
+func GCFCreteCommnet(MONGOCONNSTRINGENV, dbname, collectionname string, r *http.Request) string {
+	mconn := SetConnection(MONGOCONNSTRINGENV, dbname)
+	var commentdata Comment
+	err := json.NewDecoder(r.Body).Decode(&commentdata)
+	if err != nil {
+		return err.Error()
+	}
+
+	if err := CreateComment(mconn, collectionname, commentdata); err != nil {
+		return GCFReturnStruct(CreateResponse(true, "Succes Create Comment", commentdata))
+	} else {
+		return GCFReturnStruct(CreateResponse(false, "Failed Create Comment", commentdata))
+	}
+}
+
+func GCFGetAllComment(MONGOCONNSTRINGENV, dbname, collectionname string) string {
+	mconn := SetConnection(MONGOCONNSTRINGENV, dbname)
+	datacomment := GetAllComment(mconn, collectionname)
+	if datacomment != nil {
+		return GCFReturnStruct(CreateResponse(true, "success Get All Comment", datacomment))
+	} else {
+		return GCFReturnStruct(CreateResponse(false, "Failed Get All Comment", datacomment))
+	}
+}
+func GFCUpadatedCommnet(MONGOCONNSTRINGENV, dbname, collectionname string, r *http.Request) string {
+	mconn := SetConnection(MONGOCONNSTRINGENV, dbname)
+	var commentdata Comment
+	err := json.NewDecoder(r.Body).Decode(&commentdata)
+	if err != nil {
+		return err.Error()
+	}
+
+	if err := UpdatedComment(mconn, collectionname, bson.M{"id": commentdata.ID}, commentdata); err != nil {
+		return GCFReturnStruct(CreateResponse(true, "Success Updated Comment", commentdata))
+	} else {
+		return GCFReturnStruct(CreateResponse(false, "Failed Updated Comment", commentdata))
+	}
+}
+
+func GCFDeletedCommnet(MONGOCONNSTRINGENV, dbname, collectionname string, r *http.Request) string {
+	mconn := SetConnection(MONGOCONNSTRINGENV, dbname)
+	var commentdata Comment
+	if err := json.NewDecoder(r.Body).Decode(&commentdata); err != nil {
+		return GCFReturnStruct(CreateResponse(false, "Failed to process request", commentdata))
+	}
+
+	if err := DeleteComment(mconn, collectionname, commentdata); err != nil {
+		return GCFReturnStruct(CreateResponse(true, "Successfully deleted comment", commentdata))
+	}
+
+	return GCFReturnStruct(CreateResponse(false, "Failed to delete comment", commentdata))
+}
+
+func GCFCreatePostLineStringg(MONGOCONNSTRINGENV, dbname, collection string, r *http.Request) string {
+	mconn := SetConnection(MONGOCONNSTRINGENV, dbname)
+	var geojsonline GeoJsonLineString
+	err := json.NewDecoder(r.Body).Decode(&geojsonline)
+	if err != nil {
+		return err.Error()
+	}
+
+	// Mengambil nilai header PASETO dari permintaan HTTP
+	pasetoValue := r.Header.Get("PASETOPRIVATEKEYENV")
+
+	// Disini Anda dapat menggunakan nilai pasetoValue sesuai kebutuhan Anda
+	// Misalnya, menggunakannya untuk otentikasi atau enkripsi.
+	// Contoh sederhana menambahkan nilainya ke dalam pesan respons:
+	response := GCFReturnStruct(geojsonline)
+	response += " PASETO value: " + pasetoValue
+
+	PostLinestring(mconn, collection, geojsonline)
+	return response
+}
+
+func GCFCreatePostLineString(MONGOCONNSTRINGENV, dbname, collection string, r *http.Request) string {
+	mconn := SetConnection(MONGOCONNSTRINGENV, dbname)
+	var geojsonline GeoJsonLineString
+	err := json.NewDecoder(r.Body).Decode(&geojsonline)
+	if err != nil {
+		return err.Error()
+	}
+	PostLinestring(mconn, collection, geojsonline)
+	return GCFReturnStruct(geojsonline)
+}
+
+func GCFDeleteLineString(MONGOCONNSTRINGENV, dbname, collectionname string, r *http.Request) string {
+	mconn := SetConnection(MONGOCONNSTRINGENV, dbname)
+	var dataline GeoJsonLineString
+	err := json.NewDecoder(r.Body).Decode(&dataline)
+	if err != nil {
+		return err.Error()
+	}
+
+	if err := DeleteLinestring(mconn, collectionname, dataline); err != nil {
+		return GCFReturnStruct(CreateResponse(true, "Success Delete LineString", dataline))
+	} else {
+		return GCFReturnStruct(CreateResponse(false, "Failed Delete LineString", dataline))
+	}
+}
+
+func GCFUpdateLinestring(MONGOCONNSTRINGENV, dbname, collectionname string, r *http.Request) string {
+	mconn := SetConnection(MONGOCONNSTRINGENV, dbname)
+	var dataline GeoJsonLineString
+	err := json.NewDecoder(r.Body).Decode(&dataline)
+	if err != nil {
+		return err.Error()
+	}
+
+	if err := UpdatedLinestring(mconn, collectionname, bson.M{"properties.coordinates": dataline.Geometry.Coordinates}, dataline); err != nil {
+		return GCFReturnStruct(CreateResponse(true, "Success Update LineString", dataline))
+	} else {
+		return GCFReturnStruct(CreateResponse(false, "Failed Update LineString", dataline))
+	}
+}
+
+func GCFCreateLineStringgg(MONGOCONNSTRINGENV, dbname, collectionname string, r *http.Request) string {
+	// MongoDB Connection Setup
+	mconn := SetConnection(MONGOCONNSTRINGENV, dbname)
+
+	// Parsing Request Body
+	var dataline GeoJsonLineString
+	err := json.NewDecoder(r.Body).Decode(&dataline)
+	if err != nil {
+		return err.Error()
+	}
+
+	if r.Header.Get("Secret") == os.Getenv("SECRET") {
+		// Handling Authorization
+		err := PostLinestring(mconn, collectionname, dataline)
+		if err != nil {
+			// Success
+			return GCFReturnStruct(CreateResponse(true, "Success: LineString created", dataline))
+		} else {
+			return GCFReturnStruct(CreateResponse(false, "Error", nil))
+		}
+	} else {
+		return GCFReturnStruct(CreateResponse(false, "Unauthorized: Secret header does not match", nil))
+	}
+
+	// This part is unreachable, so you might want to remove it
+	// return GCFReturnStruct(CreateResponse(false, "Success to create LineString", nil))
+}
+
+func GCFCreatePolygone(MONGOCONNSTRINGENV, dbname, collectionname string, r *http.Request) string {
+	// MongoDB Connection Setup
+	mconn := SetConnection(MONGOCONNSTRINGENV, dbname)
+
+	// Parsing Request Body
+	var datapolygone GeoJsonPolygon
+	err := json.NewDecoder(r.Body).Decode(&datapolygone)
+	if err != nil {
+		return err.Error()
+	}
+
+	// Handling Authorization
+	if err := PostPolygone(mconn, collectionname, datapolygone); err != nil {
+		// Success
+		return GCFReturnStruct(CreateResponse(true, "Success Create Polygone", datapolygone))
+	} else {
+		// Failure
+		return GCFReturnStruct(CreateResponse(false, "Failed Create Polygone", datapolygone))
+	}
+}
+
+func GCFPoint(MONGOCONNSTRINGENV, dbname, collectionname string, r *http.Request) string {
+	mconn := SetConnection(MONGOCONNSTRINGENV, dbname)
+	var datapoint GeometryPoint
+
+	// Decode the request body
+	if err := json.NewDecoder(r.Body).Decode(&datapoint); err != nil {
+		log.Printf("Error decoding request body: %v", err)
+		return GCFReturnStruct(CreateResponse(false, "Bad Request: Invalid JSON", nil))
+	}
+
+	// Check for the "Secret" header
+	secretHeader := r.Header.Get("Secret")
+	expectedSecret := os.Getenv("SECRET")
+
+	if secretHeader != expectedSecret {
+		log.Printf("Unauthorized: Secret header does not match. Expected: %s, Actual: %s", expectedSecret, secretHeader)
+		return GCFReturnStruct(CreateResponse(false, "Unauthorized: Secret header does not match", nil))
+	}
+
+	// Attempt to post the data point to MongoDB
+	if err := PostPoint(mconn, collectionname, datapoint); err != nil {
+		log.Printf("Error posting data point to MongoDB: %v", err)
+		return GCFReturnStruct(CreateResponse(false, "Failed to create point", nil))
+	}
+
+	log.Println("Success: Point created")
+	return GCFReturnStruct(CreateResponse(true, "Success: Point created", datapoint))
+}
+
+func GCFlineStingCreate(MONGOCONNSTRINGENV, dbname, collection string, r *http.Request) string {
+	mconn := SetConnection(MONGOCONNSTRINGENV, dbname)
+	var geojsonline GeoJsonLineString
+	err := json.NewDecoder(r.Body).Decode(&geojsonline)
+	if err != nil {
+		return err.Error()
+	}
+	PostLinestring(mconn, collection, geojsonline)
+	return GCFReturnStruct(geojsonline)
+}
+
+func GCFlineStingCreatea(MONGOCONNSTRINGENV, dbname, collection string, r *http.Request) string {
+	// MongoDB Connection Setup
+	mconn := SetConnection(MONGOCONNSTRINGENV, dbname)
+
+	// Parsing Request Body
+	var geojsonline GeoJsonLineString
+	err := json.NewDecoder(r.Body).Decode(&geojsonline)
+	if err != nil {
+		return GCFReturnStruct(CreateResponse(false, "Bad Request: Invalid JSON", nil))
+	}
+
+	// Checking Secret Header
+	secretHeader := r.Header.Get("Secret")
+	expectedSecret := os.Getenv("SECRET")
+
+	if secretHeader != expectedSecret {
+		log.Printf("Unauthorized: Secret header does not match. Expected: %s, Actual: %s", expectedSecret, secretHeader)
+		return GCFReturnStruct(CreateResponse(false, "Unauthorized: Secret header does not match", nil))
+	}
+
+	// Handling Authorization
+	PostLinestring(mconn, collection, geojsonline)
+
+	return GCFReturnStruct(CreateResponse(true, "Success: LineString created", geojsonline))
+}
+
+func GCFCreatePolygonee(MONGOCONNSTRINGENV, dbname, collectionname string, r *http.Request) string {
+	// MongoDB Connection Setup
+	mconn := SetConnection(MONGOCONNSTRINGENV, dbname)
+
+	// Parsing Request Body
+	var datapolygone GeoJsonPolygon
+	err := json.NewDecoder(r.Body).Decode(&datapolygone)
+	if err != nil {
+		return GCFReturnStruct(CreateResponse(false, "Bad Request: Invalid JSON", nil))
+	}
+
+	// Checking Secret Header
+	secretHeader := r.Header.Get("Secret")
+	expectedSecret := os.Getenv("SECRET")
+
+	if secretHeader != expectedSecret {
+		log.Printf("Unauthorized: Secret header does not match. Expected: %s, Actual: %s", expectedSecret, secretHeader)
+		return GCFReturnStruct(CreateResponse(false, "Unauthorized: Secret header does not match", nil))
+	}
+
+	// Handling Authorization
+	if err := PostPolygone(mconn, collectionname, datapolygone); err != nil {
+		log.Printf("Error creating polygon: %v", err)
+		return GCFReturnStruct(CreateResponse(false, "Failed Create Polygone", nil))
+	}
+
+	log.Println("Success: Polygon created")
+	return GCFReturnStruct(CreateResponse(true, "Success Create Polygone", datapolygone))
 }
--- mongo.go
+++ mongo.go
@@ -1,6 +1,9 @@
 package BEGis
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"os"
 
 	"github.com/aiteung/atdb"
@@ -93,9 +96,183 @@ func IsPasswordValid(mongoconn *mongo.Database, collection string, userdata User
 	res := atdb.GetOneDoc[User](mongoconn, collection, filter)
 	return CheckPasswordHash(userdata.Password, res.Password)
 }
-
-// product
-
 func CreateNewProduct(mongoconn *mongo.Database, collection string, productdata Product) interface{} {
 	return atdb.InsertOneDoc(mongoconn, collection, productdata)
+}
+
+func InsertUserdata(MongoConn *mongo.Database, username, role, password string) (InsertedID interface{}) {
+	req := new(User)
+	req.Username = username
+	req.Password = password
+	req.Role = role
+	return InsertOneDoc(MongoConn, "user", req)
+}
+func InsertOneDoc(db *mongo.Database, collection string, doc interface{}) (insertedID interface{}) {
+	insertResult, err := db.Collection(collection).InsertOne(context.TODO(), doc)
+	if err != nil {
+		fmt.Printf("InsertOneDoc: %v\n", err)
+	}
+	return insertResult.InsertedID
+}
+
+// gis function
+
+// content
+func CreateNewContent(mongoconn *mongo.Database, collection string, contentdata Content) interface{} {
+	return atdb.InsertOneDoc(mongoconn, collection, contentdata)
+}
+
+func DeleteContent(mongoconn *mongo.Database, collection string, contentdata Content) interface{} {
+	filter := bson.M{"id": contentdata.ID}
+	return atdb.DeleteOneDoc(mongoconn, collection, filter)
+}
+
+func ReplaceContent(mongoconn *mongo.Database, collection string, filter bson.M, contentdata Content) interface{} {
+	return atdb.ReplaceOneDoc(mongoconn, collection, filter, contentdata)
+}
+
+func CreateNewBlog(mongoconn *mongo.Database, collection string, blogdata Blog) interface{} {
+	return atdb.InsertOneDoc(mongoconn, collection, blogdata)
+}
+
+func FindContentAllId(mongoconn *mongo.Database, collection string, contentdata Content) Content {
+	filter := bson.M{"id": contentdata.ID}
+	return atdb.GetOneDoc[Content](mongoconn, collection, filter)
+}
+
+func GetAllBlogAll(mongoconn *mongo.Database, collection string) []Blog {
+	blog := atdb.GetAllDoc[[]Blog](mongoconn, collection)
+	return blog
+}
+
+func GetIDBlog(mongoconn *mongo.Database, collection string, blogdata Blog) Blog {
+	filter := bson.M{"id": blogdata.ID}
+	return atdb.GetOneDoc[Blog](mongoconn, collection, filter)
+}
+
+func CreateUserAndAddToken(privateKeyEnv string, mongoconn *mongo.Database, collection string, userdata User) error {
+	// Hash the password before storing it
+	hashedPassword, err := HashPassword(userdata.Password)
+	if err != nil {
+		return err
+	}
+	userdata.Password = hashedPassword
+
+	// Create a token for the user
+	tokenstring, err := watoken.Encode(userdata.Username, os.Getenv(privateKeyEnv))
+	if err != nil {
+		return err
+	}
+
+	userdata.Token = tokenstring
+
+	// Insert the user data into the MongoDB collection
+	if err := atdb.InsertOneDoc(mongoconn, collection, userdata.Username); err != nil {
+		return nil // Mengembalikan kesalahan yang dikembalikan oleh atdb.InsertOneDoc
+	}
+
+	// Return nil to indicate success
+	return nil
+}
+
+func AuthenticateUserAndGenerateToken(privateKeyEnv string, mongoconn *mongo.Database, collection string, userdata User) (string, error) {
+	// Cari pengguna berdasarkan nama pengguna
+	username := userdata.Username
+	password := userdata.Password
+	userdata, err := FindUserByUsername(mongoconn, collection, username)
+	if err != nil {
+		return "", err
+	}
+
+	// Memeriksa kata sandi
+	if !CheckPasswordHash(password, userdata.Password) {
+		return "", errors.New("Password salah") // Gantilah pesan kesalahan sesuai kebutuhan Anda
+	}
+
+	// Generate token untuk otentikasi
+	tokenstring, err := watoken.Encode(username, os.Getenv(privateKeyEnv))
+	if err != nil {
+		return "", err
+	}
+
+	return tokenstring, nil
+}
+
+func FindUserByUsername(mongoconn *mongo.Database, collection string, username string) (User, error) {
+	var user User
+	filter := bson.M{"username": username}
+	err := mongoconn.Collection(collection).FindOne(context.TODO(), filter).Decode(&user)
+	if err != nil {
+		return User{}, err
+	}
+	return user, nil
+}
+
+// create login using Private
+func CreateLogin(mongoconn *mongo.Database, collection string, userdata User) interface{} {
+	// Hash the password before storing it
+	hashedPassword, err := HashPassword(userdata.Password)
+	if err != nil {
+		return err
+	}
+	userdata.Password = hashedPassword
+	// Create a token for the user
+	tokenstring, err := watoken.Encode(userdata.Username, userdata.Private)
+	if err != nil {
+		return err
+	}
+	userdata.Token = tokenstring
+
+	// Insert the user data into the database
+	return atdb.InsertOneDoc(mongoconn, collection, userdata)
+}
+
+func CreateComment(mongoconn *mongo.Database, collection string, commentdata Comment) interface{} {
+	return atdb.InsertOneDoc(mongoconn, collection, commentdata)
+}
+
+func DeleteComment(mongoconn *mongo.Database, collection string, commentdata Comment) interface{} {
+	filter := bson.M{"id": commentdata.ID}
+	return atdb.DeleteOneDoc(mongoconn, collection, filter)
+}
+
+func UpdatedComment(mongoconn *mongo.Database, collection string, filter bson.M, commentdata Comment) interface{} {
+	filter = bson.M{"id": commentdata.ID}
+	return atdb.ReplaceOneDoc(mongoconn, collection, filter, commentdata)
+}
+
+func GetAllComment(mongoconn *mongo.Database, collection string) []Comment {
+	comment := atdb.GetAllDoc[[]Comment](mongoconn, collection)
+	return comment
+}
+
+func PostLineString(mongoconn *mongo.Database, collection string, commentdata GeoJsonLineString) interface{} {
+	return atdb.InsertOneDoc(mongoconn, collection, commentdata)
+}
+
+func PostLinestring(mongoconn *mongo.Database, collection string, linestringdata GeoJsonLineString) interface{} {
+	return atdb.InsertOneDoc(mongoconn, collection, linestringdata)
+}
+
+func GetByCoordinate(mongoconn *mongo.Database, collection string, linestringdata GeoJsonLineString) GeoJsonLineString {
+	filter := bson.M{"geometry.coordinates": linestringdata.Geometry.Coordinates}
+	return atdb.GetOneDoc[GeoJsonLineString](mongoconn, collection, filter)
+}
+
+func DeleteLinestring(mongoconn *mongo.Database, collection string, linestringdata GeoJsonLineString) interface{} {
+	filter := bson.M{"geometry.coordinates": linestringdata.Geometry.Coordinates}
+	return atdb.DeleteOneDoc(mongoconn, collection, filter)
+}
+
+func UpdatedLinestring(mongoconn *mongo.Database, collection string, filter bson.M, linestringdata GeoJsonLineString) interface{} {
+	filter = bson.M{"geometry.coordinates": linestringdata.Geometry.Coordinates}
+	return atdb.ReplaceOneDoc(mongoconn, collection, filter, linestringdata)
+}
+
+func PostPolygone(mongoconn *mongo.Database, collection string, polygonedata GeoJsonPolygon) interface{} {
+	return atdb.InsertOneDoc(mongoconn, collection, polygonedata)
+}
+
+func PostPoint(mongoconn *mongo.Database, collection string, pointdata GeometryPoint) interface{} {
+	return atdb.InsertOneDoc(mongoconn, collection, pointdata)
 }
